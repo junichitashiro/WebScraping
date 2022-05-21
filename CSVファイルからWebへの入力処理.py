0a3a57d76a80c504339b4fb4b0d9af87139aabe3
junichitashiro <junichitashiro926@gmail.com>
2022-05-21 09:51:32 +0900

    Selenium 4に対応する改修

--- CSVファイルからWebへの入力処理.py
+++ CSVファイルからWebへの入力処理.py
@@ -10,7 +10,10 @@ import csv
 import os
 import tkinter.messagebox
 
-import selenium.webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service as fs
+
 
 # メッセージボックス用の設定
 root = tkinter.Tk()
@@ -41,8 +44,9 @@ else:
 # ----------------------------------------
 # ChromeDriverの設定
 # ----------------------------------------
-cd_path = 'chromedriverの絶対パス'
-chrome_options = selenium.webdriver.ChromeOptions()
+CHROMEDRIVER = 'C:\chromedriver\chromedriver.exe'
+chrome_service = fs.Service(executable_path=CHROMEDRIVER)
+chrome_options = webdriver.ChromeOptions()
 # ブラウザ表示が不要な場合はコメントインする
 # chrome_options.add_argument('--headless')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
@@ -52,7 +56,8 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-automation', 
 # 処理開始
 # ----------------------------------------
 print('>>>処理開始')
-driver = selenium.webdriver.Chrome(cd_path, options=chrome_options)
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
 driver.maximize_window()
 driver.implicitly_wait(10)
 driver.get('https://keisan.casio.jp/exec/system/1183427246/')
@@ -82,30 +87,30 @@ for i in range(1, input_row):
     act_level = line[i][2]
     weight = line[i][3]
 
-    driver.find_element_by_xpath(age_xpath).send_keys(age)
+    driver.find_element(By.XPATH, age_xpath).send_keys(age)
 
     if sex == '男':
-        driver.find_element_by_xpath(sx0_xpath).click()
+        driver.find_element(By.XPATH, sx0_xpath).click()
     elif sex == '女':
-        driver.find_element_by_xpath(sx1_xpath).click()
+        driver.find_element(By.XPATH, sx1_xpath).click()
 
     if act_level == '低い':
-        driver.find_element_by_xpath(alv1_xpath).click()
+        driver.find_element(By.XPATH, alv1_xpath).click()
     elif act_level == '高い':
-        driver.find_element_by_xpath(alv3_xpath).click()
+        driver.find_element(By.XPATH, alv3_xpath).click()
     else:
-        driver.find_element_by_xpath(alv2_xpath).click()
+        driver.find_element(By.XPATH, alv2_xpath).click()
 
-    driver.find_element_by_xpath(kg_xpath).send_keys(weight)
+    driver.find_element(By.XPATH, kg_xpath).send_keys(weight)
 
-    driver.find_element_by_xpath(execute_xpath).click()
+    driver.find_element(By.XPATH, execute_xpath).click()
 
     # 計算結果の表示
-    energy = driver.find_element_by_xpath(ans0_xpath).text
+    energy = driver.find_element(By.XPATH, ans0_xpath).text
     message = str(i) + '／' + str(input_row - 1) + '件目' + '''
     \n１日に必要なエネルギー量は ''' + energy + ' Kcalです'
     tkinter.messagebox.showinfo('計算結果', message)
-    driver.find_element_by_xpath(clear_xpath).click()
+    driver.find_element(By.XPATH, clear_xpath).click()
 
 
 # ----------------------------------------
