26934be8e4cc66963d4f7cb40cf79fe59afd653a
junichitashiro <junichitashiro926@gmail.com>
2022-06-14 12:14:25 +0900

    変数名を変更、都道府県を追加

--- weather_infomation/main.py
+++ weather_infomation/main.py
@@ -21,7 +21,7 @@ def str2float(weather_data):
 # ----------------------------------------
 # 気象データ取得処理
 # ----------------------------------------
-def scraping(url, date, area_name):
+def scraping(url, date, prec_name):
     # 気象データのページを取得
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html)
@@ -39,7 +39,7 @@ def scraping(url, date, area_name):
 
         ymd = datetime.datetime(date.year, date.month, int(tds[0].string))
         data_list.append(ymd.strftime('%Y-%m-%d'))
-        data_list.append(area_name)
+        data_list.append(prec_name)
         data_list.append(str2float(tds[1].string))
         data_list.append(str2float(tds[2].string))
         data_list.append(str2float(tds[3].string))
@@ -65,12 +65,12 @@ def scraping(url, date, area_name):
 # ----------------------------------------
 # csv出力処理
 # ----------------------------------------
-def create_csv(area_id, area_name, area_point):
+def create_csv(prec_no, prec_name, block_no):
     # csv出力先ディレクトリ
     output_dir = os.path.dirname(__file__)
 
     # 出力ファイル名
-    output_file = "weather_information_daily_" + area_name + ".csv"
+    output_file = "weather_information_daily_" + prec_name + ".csv"
 
     # データ取得開始日
     start_date = datetime.date(2020, 1, 1)
@@ -92,9 +92,9 @@ def create_csv(area_id, area_name, area_point):
 
             # 対象url
             url = "http://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?" \
-                  "prec_no=%d&block_no=%d&year=%d&month=%d&day=&view=" % (int(area_id), int(area_point), date.year, date.month)
+                  "prec_no=%d&block_no=%d&year=%d&month=%d&day=&view=" % (int(prec_no), int(block_no), date.year, date.month)
 
-            data_per_day = scraping(url, date, area_name)
+            data_per_day = scraping(url, date, prec_name)
 
             for dpd in data_per_day:
                 writer.writerow(dpd)
@@ -106,21 +106,108 @@ def create_csv(area_id, area_name, area_point):
 # メイン処理
 # ----------------------------------------
 if __name__ == '__main__':
-    area_names = {
-        '44': 'Tokyo',
-        '62': 'Osaka',
-        '51': 'Nagoya'
+    prec_info = {
+        '14': '北海道'
+        , '31': '青森県'
+        , '33': '岩手県'
+        , '34': '宮城県'
+        , '32': '秋田県'
+        , '35': '山形県'
+        , '36': '福島県'
+        , '40': '茨城県'
+        , '41': '栃木県'
+        , '42': '群馬県'
+        , '43': '埼玉県'
+        , '45': '千葉県'
+        , '44': '東京都'
+        , '46': '神奈川県'
+        , '54': '新潟県'
+        , '55': '富山県'
+        , '56': '石川県'
+        , '57': '福井県'
+        , '49': '山梨県'
+        , '48': '長野県'
+        , '52': '岐阜県'
+        , '50': '静岡県'
+        , '51': '愛知県'
+        , '53': '三重県'
+        , '60': '滋賀県'
+        , '61': '京都府'
+        , '62': '大阪府'
+        , '63': '兵庫県'
+        , '64': '奈良県'
+        , '65': '和歌山県'
+        , '69': '鳥取県'
+        , '68': '島根県'
+        , '66': '岡山県'
+        , '67': '広島県'
+        , '81': '山口県'
+        , '71': '徳島県'
+        , '72': '香川県'
+        , '73': '愛媛県'
+        , '74': '高知県'
+        , '82': '福岡県'
+        , '85': '佐賀県'
+        , '84': '長崎県'
+        , '86': '熊本県'
+        , '83': '大分県'
+        , '87': '宮崎県'
+        , '88': '鹿児島県'
+        , '91': '沖縄県'
     }
 
-    point_nums = {
-        '44': '47662',
-        '62': '47772',
-        '51': '47636'
+    block_info = {
+        '14': '47412'
+        , '31': '47575'
+        , '33': '47584'
+        , '34': '47590'
+        , '32': '47582'
+        , '35': '47588'
+        , '36': '47595'
+        , '40': '47629'
+        , '41': '47615'
+        , '42': '47624'
+        , '43': '47626'
+        , '45': '47682'
+        , '44': '47662'
+        , '46': '47670'
+        , '54': '47604'
+        , '55': '47607'
+        , '56': '47605'
+        , '57': '47616'
+        , '49': '47638'
+        , '48': '47610'
+        , '52': '47632'
+        , '50': '47656'
+        , '51': '47636'
+        , '53': '47651'
+        , '60': '47761'
+        , '61': '47759'
+        , '62': '47772'
+        , '63': '47770'
+        , '64': '47780'
+        , '65': '47777'
+        , '69': '47746'
+        , '68': '47741'
+        , '66': '47768'
+        , '67': '47765'
+        , '81': '47784'
+        , '71': '47895'
+        , '72': '47891'
+        , '73': '47887'
+        , '74': '47893'
+        , '82': '47807'
+        , '85': '47813'
+        , '84': '47817'
+        , '86': '47819'
+        , '83': '47815'
+        , '87': '47830'
+        , '88': '47827'
+        , '91': '47936'
     }
 
-    for area_key in area_names.keys():
-        area_id = area_key
-        area_name = area_names[area_key]
-        area_point = point_nums[area_key]
+    for prec_no in prec_info.keys():
+        prec_name = prec_info[prec_no]
+        block_no = block_info[prec_no]
 
-        create_csv(area_id, area_name, area_point)
+        create_csv(prec_no, prec_name, block_no)
